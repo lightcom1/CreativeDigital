0cc1fa096b0124de6d696f7247a2cae20b68d690
lightcom1 <69343253+lightcom1@users.noreply.github.com>
2020-09-17 21:44:35 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ $(function () {
 	    $('.footer').each(function(){
 	        var imagePos = $(this).offset().top;
 	        var topOfWindow = $(window).scrollTop();
-	        if (imagePos < topOfWindow+770) {
+	        if (imagePos < topOfWindow+700) {
 	            $(this).addClass("animate__fadeInUp");
 	        }
 	    });
